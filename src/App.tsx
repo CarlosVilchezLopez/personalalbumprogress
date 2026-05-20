3fa8539b98fb75217a66a2dcf9655c6201294aad
sanmoncho <carlosvilchezllopez@gmail.com>
2026-05-20 12:55:28 -0500

    chore: scaffold panini tracker app

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <main className="app-shell">
+      <h1>Album Panini Mundial 2026</h1>
+      <p>Preparando tu tracker local del album.</p>
+    </main>
+  );
+}
